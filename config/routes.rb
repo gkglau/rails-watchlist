169dcf1e54967ef2b385d453b0ff385925b79f03
Glaucia Grossi <glau.kobashigawa.jp@gmail.com>
2026-02-20 15:09:25 +0900

    destroy/delete list created

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # [ ] - As a user, I can bookmark a movie inside a list
   # [ ] - As a user, I can destroy a bookmark
   root to: "lists#index"
-  resources :lists, only: [ :index, :show, :new, :create ] do
+  resources :lists, only: [ :index, :show, :new, :create, :destroy ] do
     resources :bookmarks, only: [ :new, :create ]
   end
 end
